bd10eb46b269fe9a3bd5ce9027152580d887f719
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-03-28 00:18:11 -0300

    Update app.js

--- pro-franchising/app.js
+++ pro-franchising/app.js
@@ -1,23 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import theme from './styles/theme';
+import ErrorBoundary from './components/ErrorBoundary';
+import Layout from './components/Layout';
 import Home from './components/Home';
 import ProductsPage from './components/ProductsPage';
 import CartPage from './components/CartPage';
 
+// Configuração do cliente de consulta
 const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* Provedor de consulta */}
     <QueryClientProvider client={queryClient}>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/products" component={ProductsPage} />
-          <Route path="/cart" component={CartPage} />
-        </Switch>
-      </Router>
+      {/* Provedor de tema */}
+      <ThemeProvider theme={theme}>
+        {/* Estilos globais */}
+        <GlobalStyles />
+        {/* Componente de limite de erro global */}
+        <ErrorBoundary>
+          {/* Router */}
+          <Router>
+            {/* Layout comum para todas as páginas */}
+            <Layout>
+              {/* Switch para roteamento */}
+              <Switch>
+                {/* Rota para página inicial */}
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                {/* Rota para página de produtos */}
+                <Route path="/products">
+                  <ProductsPage />
+                </Route>
+                {/* Rota para página do carrinho */}
+                <Route path="/cart">
+                  <CartPage />
+                </Route>
+                {/* Rota para páginas não encontradas */}
+                <Route path="*">
+                  <Redirect to="/" />
+                </Route>
+              </Switch>
+            </Layout>
+          </Router>
+        </ErrorBoundary>
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
