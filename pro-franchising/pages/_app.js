cb4c8dfe3a659049f826e4d4850504facca3b7d1
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-03-28 00:15:31 -0300

    Update _app.js

--- pro-franchising/pages/_app.js
+++ pro-franchising/pages/_app.js
@@ -5,10 +5,34 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
+  // Tratamento de erros global
+  window.addEventListener('unhandledrejection', event => {
+    console.error('Erro não tratado:', event.reason);
+  });
+
+  // Estado de carregamento inicial
+  const [loading, setLoading] = React.useState(true);
+
+  // Definindo um tempo de carregamento inicial
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Carregamento inicial de 2 segundos
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
-        <Component {...pageProps} />
+        {/* Renderização condicional para mostrar um spinner de carregamento inicial */}
+        {loading ? (
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <h1>Carregando...</h1>
+          </div>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </Router>
     </QueryClientProvider>
   );
