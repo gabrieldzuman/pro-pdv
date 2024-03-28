742330527e56f987c41d4fbf0dd547c7e9f61c7a
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-03-28 00:13:47 -0300

    Update PrivateRoute.js

--- pro-franchising/pages/api/PrivateRoute.js
+++ pro-franchising/pages/api/PrivateRoute.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-function PrivateRoute({ children, token, ...rest }) {
+// Componente PrivateRoute é uma rota protegida que requer autenticação para acessá-la
+function PrivateRoute({ children, token, tokenExpiration, ...rest }) {
+  // Verifica se o token está presente e se não expirou
+  const isAuthenticated = token && new Date(tokenExpiration) > new Date();
+
   return (
     <Route
       {...rest}
       render={({ location }) =>
-        token ? (
+        isAuthenticated ? (
+          // Se o usuário estiver autenticado e o token não tiver expirado, renderiza os componentes filhos
           children
         ) : (
+          // Se não estiver autenticado ou o token tiver expirado, redireciona para a página de login
           <Redirect
             to={{
-              pathname: '/login',
-              state: { from: location }
+              pathname: '/login', // Redireciona para a página de login
+              state: {
+                from: location, // Passa o local atual como estado para redirecionamento após o login
+                expiredToken: !token // Indica se o token expirou
+              }
             }}
           />
         )
